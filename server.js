de715bc9f4b882edafe82e6d33fd179c9d7352e6
Aidan <aidan.burke1@mohawkcollege.ca>
2024-09-29 04:02:45 -0400

    asdfasdfasdfasdfasdf

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// I, Aidan Burke, student number 000847978, certify that this material is my
-// original work. No other person's work has been used without due
-// acknowledgement and I have not made my work available to anyone else.
+// StAuth10222: I Aidan Burke, 000847978 certify that this material is my original work.
+// No other person's work has been used without due acknowledgement.
+// I have not made my work available to anyone else.
 
-const e = require('express');
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 
@@ -12,36 +11,18 @@ port = 3000;
 
 app.use(express.json());
 
+
+// set up db
 db.serialize(() => {
     db.run("drop table if exists movies");
     db.run("create table movies (title text, release_year integer, timestamp text)");
-    const movies = [
-        {
-            "title":"Gladiator",
-            "release_year":"2000",
-            "timestamp":"2017-10-03T11:45:56.200"
-        },
-        {
-            "title":"Avengers: Infinity War",
-            "release_year":"2019",
-            "timestamp":"2019-12-03T15:20:20.200"
-        },
-        {
-            "title":"Wonder Woman",
-            "release_year":"2017",
-            "timestamp":"2017-06-04T08:45:56.200"
-        }
-    ]
-    movies.forEach((movie) => {
-        db.run("insert into movies (title, release_year, timestamp) values (?,?,?)",
-            [movie.title, movie.release_year, movie.timestamp]);
-    });
 });
 
+// GET collection
 app.get('/api', (req, res) => {
     db.all("select * from movies", (err, results) => {
         if (err) {
-            res.status(400).json({error: err.message});
+            res.json({error: err.message});
         }
         else {
             res.json(results);
@@ -49,20 +30,117 @@ app.get('/api', (req, res) => {
     });
 });
 
+
+// PUT collection
 app.put('/api', (req, res) => {
-    const data = req.body;
-    db.serialize(() => {
-        db.run("drop table if exists movies");
-        db.run("create table movies (title text, release_year integer, timestamp text)");
+    if(!req.body) {
+        res.json({error: "INVALID INPUT"});
+        return;
+    }
+    db.run("delete from movies", (err, results) => {
+        if (err) {
+            res.json({error: err.message});
+        }
+        else {
+            db.serialize(() => {
+                for (let item of req.body) {
+                    db.run("insert into movies (title, release_year, timestamp) values (?,?,?)",
+                        [item.title, item.release_year, item.timestamp],
+                        (err, results) => {
+                            if (err) {
+                                res.json({error: err.message});
+                            }
+                        }
+                    );
+                }
+            });
+            res.json({status: "REPLACE COLLECTION SUCCESSFUL"});
+        }
     });
-    data.forEach((movie) => {
-        db.run("insert into movies (title, release_year, timestamp) values (?,?,?)",
-            [movie.title, movie.release_year, movie.timestamp]);
+});
+
+// DELETE collection
+app.delete('/api', (req, res) => {
+    db.run("delete from movies", (err, results) => {
+        if (err) {
+            res.json({error: err.message});
+        }
+        else {
+            res.json({status: "DELETE COLLECTION SUCCESSFUL"});
+        }
     });
-    res.status(200).json({status: "REPLACE COLLECTION SUCCESSFUL"});
 });
 
 
+// GET single entry
+app.get('/api/:id', (req, res) => {
+    db.get("select * from movies where rowid = ?", [req.params.id], 
+        (err, results) => {
+            if (err) {
+                res.json({error: err.message});
+            }
+            else {
+                res.json(results);
+            }
+        }
+    );
+});
+
+//POST single entry
+app.post('/api', (req, res) => {
+    const data = req.body;
+    if (!data.title || !data.release_year || !data.timestamp) {
+        res.json({error: "INVALID INPUT"});
+        return;
+    }
+    db.run("insert into movies (title, release_year, timestamp) values (?,?,?)",
+        [data.title, data.release_year, data.timestamp],
+        (err, results) => {
+            if (err) {
+                res.json({error: err.message});
+            }
+            else {
+                res.json({status: "CREATE ENTRY SUCCESSFUL"});
+            }
+        }
+    );
+});
+
+// PUT single entry
+app.put('/api/:id', (req, res) => {
+    const data = req.body;
+    if (!data.title || !data.release_year || !data.timestamp) {
+        res.json({error: "INVALID INPUT"});
+        return;
+    }
+    db.run("update movies set title=?, release_year=?, timestamp=? where rowid=?",
+        [data.title, data.release_year, data.timestamp, req.params.id],
+        (err, results) => {
+            if (err) {
+                res.json({error: err.message});
+            }
+            else {
+                res.json({status: "UPDATE ITEM SUCCESSFUL"});
+            }
+        }
+    );
+});
+
+// DELETE single entry
+app.delete('/api/:id', (req, res) => {
+    db.run("delete from movies where rowid=?", [req.params.id], 
+        (err, results) => {
+            if (err) {
+                res.json({error: err.message});
+            }
+            else {
+                res.json({status: "DELETE ITEM SUCCESSFUL"});
+            }
+        }
+    );
+});
+
+// start server
 app.listen(port, () => {
     console.log(`Server running at localhost:${port}`);
 });
